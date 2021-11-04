66a7f58b2182841632a2aec789311c417579ce74
Andrey <shilovish_AV@tut.by>
2021-11-04 22:53:06 +0300

    Start my course project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.constructioncostpm {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.constructioncostpm to javafx.fxml;
+    exports com.example.constructioncostpm;
+}
